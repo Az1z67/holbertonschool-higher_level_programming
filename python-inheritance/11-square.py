b4b0e3965f36248cc34a6ec90d43f48a4dd142d8
Az1z67 <abbasovaziz61@gmail.com>
2024-06-02 16:59:54 +0400

    commit

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
-Square = __import__('11-square').Square
+"""Square Class"""
 
-s = Square(13)
 
-print(s)
-print(s.area())
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Define Square size"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """for area"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """str"""
+        c = __class__.__name__
+        return ("[{}] {}/{}".format(c, self.__size, self.__size))
