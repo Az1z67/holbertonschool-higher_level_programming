ace3b6279574b2894eeb1917dc4bf5c416e0e192
Az1z67 <abbasovaziz61@gmail.com>
2024-06-07 12:57:13 +0400

    commit

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"Append to the file"
+"""Append to the file"""
 
 
 def write_file(filename="", text=""):
-    "appends a string to a text file"
+    """appends a string to a text file"""
     with open(filename, "a") as f:
         return f.write(text)
