165d44e71fef6608dca84bc092e3b24fe2b00357
Az1z67 <abbasovaziz61@gmail.com>
2024-06-08 15:50:26 +0400

    11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,42 +1,24 @@
 #!/usr/bin/python3
-import os
-import sys
-
-Student = __import__('11-student').Student
-read_file = __import__('0-read_file').read_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-path = sys.argv[1]
-
-if os.path.exists(path):
-    os.remove(path)
-
-student_1 = Student("John", "Doe", 23)
-j_student_1 = student_1.to_json()
-print("Initial student:")
-print(student_1)
-print(type(student_1))
-print(type(j_student_1))
-print("{} {} {}".format(student_1.first_name, student_1.last_name, student_1.age))
+"""Defines a class Student."""
 
 
-save_to_json_file(j_student_1, path)
-read_file(path)
-print("\nSaved to disk")
+class Student:
+    """Represent a student."""
 
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-print("Fake student:")
-new_student_1 = Student("Fake", "Fake", 89)
-print(new_student_1)
-print(type(new_student_1))
-print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
 
-
-print("Load dictionary from file:")
-new_j_student_1 = load_from_json_file(path)
-
-new_student_1.reload_from_json(j_student_1)
-print(new_student_1)
-print(type(new_student_1))
-print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
+    def reload_from_json(self, json):
+        """Replace all attributes of the Students"""
+        for k, v in json.items():
+            setattr(self, k, v)
