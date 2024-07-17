3b2711f27ccebc00b6b29b449f8134a5fdd4f8ce
Az1z67 <abbasovaziz61@gmail.com>
2024-07-17 18:49:09 +0400

    commit

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,4 +1,4 @@
-from os.path import exists
+from os import exists
 
 
 def generate_invitations(template, attendees_list):
@@ -31,9 +31,3 @@ def generate_invitations(template, attendees_list):
         else:
             print("File already exists")
             
-            
-attendees_list = [
-    {"name": "Alice", "event_title": "Python Conference", "event_date": "2023-07-15", "event_location": "New York"},
-    {"name": "Bob", "event_title": "Data Science Workshop", "event_date": "2023-08-20", "event_location": "San Francisco"},
-    {"name": "Charlie", "event_title": "AI Summit", "event_date": None, "event_location": "Boston"}
-]
