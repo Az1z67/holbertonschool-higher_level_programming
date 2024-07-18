e4b59311f629708407d84ee31022dd62d83846db
Az1z67 <abbasovaziz61@gmail.com>
2024-07-18 13:27:21 +0400

    commit

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -51,5 +51,24 @@ def items():
 
     return render_template('items.html', items=items)
 
+@app.route('/products')
+def products():
+    source = request.args.get("source", None)
+    product_id = request.args.get("id", None)
+    
+    if source == 'json':
+        file_path = 'products.json'
+        products = read_from_json(file_path)
+
+    elif source == 'csv':
+        file_path = 'products.csv'
+        products = read_from_csv(file_path)
+
+    else:
+        return render_template('product_display.html', error="Wrong source")
+
+    if product_id:
+        products = [p for p in products if p['id'] == product_id]
+    
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
