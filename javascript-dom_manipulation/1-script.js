a77c44c05c178780bf43b0a8fc39b7d7363291a3
Az1z67 <abbasovaziz61@gmail.com>
2024-07-28 22:54:39 +0400

    commit

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const headColor = document.querySelector('header');
+
+const setColor = document.querySelector('#red_header');
+setColor.addEventListener('click', () => {
+  headColor.classList.add('red');
+});
